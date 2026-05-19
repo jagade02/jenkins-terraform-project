f09bae0b794dc96102a610cd3569d588134240eb
jagade02 <sejaljagade02@gmail.com>
2026-05-19 19:54:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "jenkins-terraform-secure-bucket-sejal-2026"
+provider "aws" {
+  region = "ap-south-1" # Mumbai Region
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-03f4878755434977f" # Ha ap-south-1 madhla standard Amazon Linux 3 AMI ID aahe
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Created-EC2"
+  }
 }

07501ddd69769601b5671317d4693cdaaa5be3fc
jagade02 <sejaljagade02@gmail.com>
2026-05-20 12:12:35 +0530

    updated code to use IAM role instead of keys

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "ap-south-1" 
 }
 
-resource "aws_instance" "my_jenkins_server" {
+resource "aws_instance" "my_new_server" {
   ami           = "ami-007020fd9c84617ee" 
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform-Jenkins-EC2"
+    Name = "Launched-By-Jenkins-Automated"
   }
 }
